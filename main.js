13c3526d133a3a4217f3027c6af24052520467dc
franco <franco.liu@dxc.com>
2021-11-17 01:03:46 +0800

    up

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ let app = express();
 app.get('/getfile', function (req, res) {
     
   console.log("/getfile");
-  const file = `photos/${req.query.file}`;
+  const file = `files/${req.query.file}`;
   res.download(file); // Set disposition and send it.
 })
+ 
 let port = 3000;
 app.listen(port);
